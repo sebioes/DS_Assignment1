04b39a0149cf39ec0ee16b33e593b9e1fc50ba7b
Sanjiv Jha <jhasanjiv5@gmail.com>
2023-09-26 02:48:47 +0200

    Refactor: add output of task2 and update test script

--- Tasks/app/src/main/java/io/grpc/filesystem/test/CheckOutput.java
+++ Tasks/app/src/main/java/io/grpc/filesystem/test/CheckOutput.java
@@ -1,9 +1,12 @@
 package io.grpc.filesystem.test;
-import java.io.BufferedReader;
+
+import java.io.*;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
-/* 
+/*
  * Using this file you can check your output against the expected output file. For both the tasks 2 and 3 the answers are expected to be the same.
  * Please use the this file to compare your output against the expected output available at output/output.txt.
  * It's possible that the word arrangement with the same count in the text file provides incorrect results.
@@ -13,31 +16,36 @@ import java.io.IOException;
 public class CheckOutput {
     public static void main(String[] args) throws IOException{
         String outputFilePath = args[0];
-        BufferedReader bf1 = new BufferedReader(new FileReader("output/output.txt"));
-        BufferedReader bf2 = new BufferedReader(new FileReader(outputFilePath));
-        String l2 = bf2.readLine();
         String l1;
-        boolean areSame = false;
-        while((l1 = bf1.readLine()) != null || l2 != null){
-            if (l1 == null && l2.isEmpty() || l1.isEmpty() && l2 == null){
-                areSame = true;
-            }else
-            if(l1.equals(l2)){
-                areSame = true;
-            }else{
-                areSame = false;
-                break;
+        String l2;
+
+        // Read and map the original file
+        Map<String, Integer> original = new HashMap<>();
+        BufferedReader bf1 = new BufferedReader(new FileReader("output/output.txt"));
+        while((l1=bf1.readLine()) != null) {
+            if(!l1.isEmpty()) {
+                String[] subStrings = l1.split(":");
+                original.put(subStrings[0], Integer.parseInt(subStrings[1]));
             }
-
-            l2=bf2.readLine();
-
         }
-        if (areSame){
+
+        // Read and map the file that needs to be compared
+        Map<String, Integer> copy = new HashMap<>();
+        BufferedReader bf2 = new BufferedReader(new FileReader(outputFilePath));
+        while((l2=bf2.readLine()) != null) {
+            if(!l2.isEmpty()) {
+                String[] subStrings = l2.split(":");
+                copy.put(subStrings[0], Integer.parseInt(subStrings[1]));
+            }
+        }
+
+        // Print the verdict
+        if(original.equals(copy)){
             System.out.println("Both files are the same!");
+        }else{
+            System.out.println("Both files are NOT the same!");
         }
-        else{
-            System.out.println("These lines are not the same: "+l1+", "+l2);
-        }
+
         bf1.close();
         bf2.close();
 
